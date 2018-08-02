8e6d191aeefc98afb2663ecd3dbd9ca07ee84895
Klvst3r <klvst3r@gmail.com>
2018-08-02 18:24:26 -0500

    Fix user name with upercase the first latter

--- myProject/tests/Feature/WelcomeUsersTest.php
+++ myProject/tests/Feature/WelcomeUsersTest.php
@@ -11,17 +11,17 @@ class WelcomeUsersTest extends TestCase
     /**
      * @test
      */
-    public function it_welcomes_users_with_nickname()
+     function it_welcomes_users_with_nickname()
     {
         $this->get('saludo/patron/klvst3r')
         ->assertStatus(200)
-        ->assertSee('Bienvenido patron, tu nickname es: klvst3r');
+        ->assertSee('Bienvenido Patron, tu nickname es: Klvst3r');
     }
 
     /**
      * @test
      */
-	public function it_welcomes_users_without_nickname()
+	 function it_welcomes_users_without_nickname()
     {
        	$this->get('saludo/patron')
         ->assertStatus(200)
