89433ddb7e48d973af59d95ecc9fe924ce09cdad
Klvst3r <klvst3r@gmail.com>
2018-08-02 17:44:01 -0500

    Test to check user details woth id 5

--- myProject/tests/Feature/UserModuleTest.php
+++ myProject/tests/Feature/UserModuleTest.php
@@ -18,4 +18,16 @@ class UserModuleTest extends TestCase
         ->assertStatus(200)
         ->assertSee('Usuarios');
     }
+
+    /**
+     * @test
+     */
+    function it_loads_the_user_details_page()
+    {
+    	$this->get('/usuarios/5')
+    	->assertStatus(200)
+    	->assertSee('Mostrando detalle del usuario: 5');
+    }
+
+    
 }
