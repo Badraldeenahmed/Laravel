d71ba825eca9cf77441533734ba48c8f56102ffc
Klvst3r <klvst3r@gmail.com>
2018-08-23 11:50:54 -0500

    Change check foreign key to Global Databases Seeder

--- myProject/database/seeds/ProfessionSeeder.php
+++ myProject/database/seeds/ProfessionSeeder.php
@@ -13,10 +13,6 @@ class ProfessionSeeder extends Seeder
     public function run()
     {
 
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-
-    	DB::table('professions')->truncate();
-
         DB::table('professions')->insert([
         	'title' => 'Desarrollador Back-end',
         ]);
